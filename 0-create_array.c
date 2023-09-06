b2e1282f08f29fc43335ee9f5c3af5847e043079
Rebecca1dah <rebeccadahzannou62@gmail.com>
2023-09-06 13:32:12 -0700

    mailloc,free

--- 0-create_array.c
+++ 0-create_array.c
@@ -1,7 +1,14 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - create array of size size and assign char c
+ * @size: size of array
+ * @c: char to assign
+ * Description: create array of size size and assign char c
+ * Return: pointer to array, NULL if fail
+ *
+ */
 char *create_array(unsigned int size, char c)
 {
 	char *str;
@@ -15,4 +22,5 @@ char *create_array(unsigned int size, char c)
 		str[i] = c;
 	return (str);
 }
+
 
