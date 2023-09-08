c8d15c70672ae071b74bc31953af0f1e0862714c
Rebecca1dah <rebeccadahzannou62@gmail.com>
2023-09-08 07:44:18 -0700

    4 à 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -59,12 +59,12 @@ int main(int argc, char *argv[])
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2)
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		error();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len)
+	result = malloc(sizeof(int) * len);
 		if (!result)
 			return (1);
 	for (i = 0; i <= len1 + len2; i++)
@@ -93,6 +93,6 @@ int main(int argc, char *argv[])
 	if (!a)
 		_putchar('0');
 	_putchar('\n');
-	free(result)
+	free(result);
 		return (0);
 }
