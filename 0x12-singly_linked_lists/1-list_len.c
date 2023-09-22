260ed6ed2cb2c51d510e6ab8e9c07731a21aed5f
Rebecca1dah <rebeccadahzannou62@gmail.com>
2023-09-22 10:01:37 -0700

    0 à 4

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,9 +3,9 @@
 
 /**
  * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
+ * @r: pointer to the list_t list
  *
- * Return: number of elements in h
+ * Return: number of elements in r
  */
 size_t list_len(const list_t *r)
 {
